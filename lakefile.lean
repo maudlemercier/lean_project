1c320b6155458fb397b9fd891acb3a966afdbb43
Maud Lemercier <m.lemercier@logosresearch.ai>
2025-10-21 16:54:00 +0100

    local mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -8,8 +8,9 @@ package «lean_project» where
   ]
   -- add any additional package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require mathlib from "../mathlib4"
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib «LeanProject» where
